29e83a8b7fe400c21fb9975c039909ebac6fefe7
BpHub4vf <122996247+BpHub4vf@users.noreply.github.com>
2023-01-18 20:47:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001895151847').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
+auth_channel = environ.get('-1001685314725')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
@@ -40,8 +40,8 @@ FILDLT_CNL = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Bhargav7016:bhargavp7016@cluster0.weineer.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "HUB4VFBot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'HUB4VFFILE')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Videofilefilter_Bot")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TELEGRAM_FILE')
 
 # Channel Button Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/MEDIAMHUB')
