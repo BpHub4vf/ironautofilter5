46de7bd75e5be469edcad2785efaa543b1e3512e
BpHub4vf <122996247+BpHub4vf@users.noreply.github.com>
2023-04-12 11:42:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get(API_ID', '24736263'))
+API_ID = int(environ.get('API_ID', '24736263'))
 API_HASH = environ.get('API_HASH', '4d53732917b73a6bb89c3b2f2f7b0902')
 BOT_TOKEN = environ.get('BOT_TOKEN', "6156393446:AAEj4SJHEkOMs_jGG4FrYIoSO7cVkheuU74")
 
